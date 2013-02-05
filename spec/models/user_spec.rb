dde21fd8d9a90da001c759187cb0391d99fc8334
Sebastien Dumont <dumontsebastien@gmail.com>
2013-02-04 22:39:48 -0500

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,6 +28,7 @@ describe User do
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
+  it { should_not be_admin }
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -109,6 +110,15 @@ describe User do
 	  before { @user.password = @user.password_confirmation = "a" * 5 }
 	  it { should be_invalid }
 	end
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 end
 
 describe "remember token" do
